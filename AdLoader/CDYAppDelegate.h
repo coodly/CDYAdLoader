24d31ce7ab62a2ef7feaf258e15ebd4f5cc72386
Jaanus Siim <jaanus@jaanussiim.com>
2014-03-30 22:09:30 +0300

    Initial project files

---
+++ AdLoader/CDYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CDYAppDelegate.h
+//  AdLoader
+//
+//  Created by Jaanus Siim on 30/03/14.
+//  Copyright (c) 2014 Coodly OÜ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AdLoader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AdLoader
+//
+//  Created by Jaanus Siim on 30/03/14.
+//  Copyright (c) 2014 Coodly OÜ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CDYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CDYAppDelegate class]));
+    }
+}
